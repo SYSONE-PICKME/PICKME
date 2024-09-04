7f7d59d73cb3009b8db672682224c52efd3bd491
YangSoeun <soeun8636@naver.com>
2024-09-04 17:23:16 +0900

    [FEAT]: 프로젝트 생성

---
+++ pickme/pickme/src/main/java/project/pickme/PickmeApplication.java
@@ -0,0 +1,13 @@
+package project.pickme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickmeApplication.class, args);
+	}
+
+}
---
+++ pickme/pickme/src/test/java/project/pickme/PickmeApplicationTests.java
@@ -0,0 +1,13 @@
+package project.pickme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
