2319e4d81884bef6f0f055fdb98e5f2f81e00f30
YangSoeun <soeun8636@naver.com>
2024-09-09 17:44:59 +0900

    [REMOVE]: 사용안하는 메소드, 클래스 삭제

--- pickme/src/main/java/project/pickme/user/controller/CustomsController.java
+++ pickme/src/main/java/project/pickme/user/controller/CustomsController.java
@@ -6,25 +6,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-import project.pickme.common.annotation.CurrentUser;
-import project.pickme.user.domain.Customs;
 import project.pickme.user.dto.LoginDto;
-import project.pickme.user.service.CustomsService;
 
 @RequestMapping("/customs")
 @Controller
 @RequiredArgsConstructor
 public class CustomsController {
-	private final CustomsService customsService;
-
 	@GetMapping("/loginForm")
 	public String loginForm(@ModelAttribute("loginDto") LoginDto loginDto){
 		return "customs/loginForm";
-	}
-
-	@GetMapping()
-	public String test(@CurrentUser Customs customs){
-		System.out.println(customs.toString());
-		return "home";
 	}
 }
--- pickme/src/main/java/project/pickme/user/service/CustomsService.java
+++ pickme/src/main/java/project/pickme/user/service/CustomsService.java
@@ -3,7 +3,6 @@ import java.util.List;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
