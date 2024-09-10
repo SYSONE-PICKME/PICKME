0f650269ff5c4b15ba67978fbe216ac81b8f875d
YangSoeun <soeun8636@naver.com>
2024-09-10 17:05:46 +0900

    [TEST]: 입찰 저장하는 메소드 테스트 코드 작성

---
+++ pickme/src/test/java/project/pickme/bid/repository/BidMapperTest.java
@@ -0,0 +1,71 @@
+// package project.pickme.bid.repository;
+//
+// import static org.assertj.core.api.Assertions.*;
+// import static project.pickme.user.constant.Type.*;
+//
+// import java.util.List;
+//
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.DisplayName;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.transaction.annotation.Transactional;
+//
+// import project.pickme.bid.domain.Bid;
+// import project.pickme.bid.dto.BidCreateDto;
+// import project.pickme.user.constant.Role;
+// import project.pickme.user.domain.User;
+// import project.pickme.user.repository.UserMapper;
+//
+// @SpringBootTest
+// @ActiveProfiles("test")
+// class BidMapperTest {
+// 	@Autowired private BidMapper bidMapper;
+// 	@Autowired private UserMapper userMapper;
+//
+// 	@BeforeEach
+// 	void setUp(){
+// 		//TODO: 공매 품 저장 해야함 지금은 db에 데이터 넣었음
+// 		User user = createUser("testUser");
+// 		userMapper.save(user);
+// 	}
+//
+// 	@AfterEach
+// 	void tearDown() {
+// 		bidMapper.deleteAll();
+// 		userMapper.deleteAll();
+// 	}
+//
+// 	@Test
+// 	@DisplayName("입찰을 저장할 수 있다.")
+// 	void save() {
+// 	    // given
+// 		BidCreateDto bid = BidCreateDto.create(1000, "testUser", 1l);
+//
+// 		// when
+// 		bidMapper.save(bid);
+//
+// 	    // then
+// 		List<Bid> findBids = bidMapper.findAll();
+// 		assertThat(findBids).hasSize(1)
+// 			.extracting("price", "user.id", "isSuccess", "item.id")
+// 			.containsExactlyInAnyOrder(tuple(1000l, "testUser", false, 1l));
+// 	}
+//
+// 	private static User createUser(String id) {
+// 		return User.builder()
+// 			.id(id)
+// 			.password("1234")
+// 			.role(Role.ROLE_USER)
+// 			.name("김테스트")
+// 			.email("test@naver.com")
+// 			.type(USER)
+// 			.addr("서울특별시 종로구 창경궁로")
+// 			.phoneNum("010-1111-1111")
+// 			.businessNum(null)
+// 			.build();
+// 	}
+// }
