282755a68d1063844aa70f4bd163e0f60a2b6303
hamin-yeong <mytokki0323@gmail.com>
2024-09-11 18:06:33 +0900

    [FEAT]: ItemFormDto구현

---
+++ pickme/src/main/java/project/pickme/item/domain/ItemFormDTO.java
@@ -0,0 +1,31 @@
+package project.pickme.item.domain;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import project.pickme.user.constant.Type;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemFormDTO {
+	private String name;
+	private String code;
+	private Type target;
+	private Long price;
+	private LocalDate startDate;
+	private LocalTime startTime;
+	private LocalDate endDate;
+	private LocalTime endTime;
+
+	private int law;
+
+}
