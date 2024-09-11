f09baed4c0d68b1bc6efecabb584b03588359028
YangSoeun <soeun8636@naver.com>
2024-09-11 17:15:46 +0900

    [FEAT]: 보유 포인트 보다 제시한 금액이 큰지 확인하는 기능 구현
    - 보유 포인트 부족, 금액 미입력, 최고가 보다 적은 금액 입력 시 모달창 띄우도록 모달창 화면 + 기능 구현

--- pickme/src/main/resources/static/js/bid/socket.js
+++ pickme/src/main/resources/static/js/bid/socket.js
@@ -1,7 +1,9 @@
+import Modal from './modal.js';
+
 function updateMaxPrice(maxPrice){
     const maxPriceElement = document.querySelector('.max-price');
     console.log("최고가 업데이트: ", maxPrice);
-    maxPriceElement.textContent = `${maxPrice} 원`;
+    maxPriceElement.textContent = `${maxPrice}`;
 
     maxPriceElement.classList.add('updated');
 
@@ -22,12 +24,11 @@ function sendBidToServer(socket, price, itemId, userId){
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    const modal = new Modal("modal", "modal-message", "modal-confirm");
     //소켓 생성(웹소켓 메서드와 이벤트를 통해 서버와 통신 가능)
     const socket = new WebSocket(`ws://localhost:8099/connect/${itemId}/${userId}`);
-    const modal = document.getElementById("modal");
 
     socket.onopen = () => console.log("웹 소켓 open");
-
     socket.onmessage = (event) => {
         const data = JSON.parse(event.data); // 서버로부터 받은 데이터를 JSON 객체로 파싱
         console.log("서버로부터 받음: ", data);
@@ -36,7 +37,6 @@ document.addEventListener("DOMContentLoaded", function () {
             updateMaxPrice(data.maxPrice);
         }
     };
-
     socket.onerror = (error) => console.log("에러 발생:", error);
     socket.onclose = () => console.log("웹 소켓 닫힘");
 
@@ -46,15 +46,16 @@ document.addEventListener("DOMContentLoaded", function () {
         const maxPriceElement = document.querySelector('.max-price');
         const currentMaxPrice = parseInt(maxPriceElement.textContent);
 
-        if(price){
-            if(parseInt(price) > currentMaxPrice){
+        if (price) {
+            if (parseInt(price) < currentMaxPrice) {
+                modal.open("최고가 보다 높은 금액을 제시해야 합니다.")
+            } else if(parseInt(price) > myPoint){
+                modal.open("보유 포인트가 부족합니다.")
+            } else {
                 sendBidToServer(socket, price, itemId, userId);
-            } else{
-                console.log("현재 최고가 보다 높은 금액을 제시해야 합니다.");
-                modal.style.display = "block";
             }
-        } else{
-            alert('금액을 입력해주세요.');
+        } else {
+            modal.open("금액을 입력해주세요.")
         }
     })
 })
