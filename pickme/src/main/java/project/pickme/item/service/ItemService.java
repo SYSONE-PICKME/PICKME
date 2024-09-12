c6ae4e0883002ae90670f866d30176118caa3093
hamin-yeong <mytokki0323@gmail.com>
2024-09-12 21:41:41 +0900

    [FEAT]: 공매 물품 등록 구현(이미지 제외)

---
+++ pickme/src/main/java/project/pickme/item/service/ItemService.java
@@ -0,0 +1,34 @@
+package project.pickme.item.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import project.pickme.item.constant.Status;
+import project.pickme.item.dto.ItemDto;
+import project.pickme.item.dto.ItemFormDto;
+import project.pickme.item.dto.LawDto;
+import project.pickme.item.repository.ItemMapper;
+import project.pickme.item.repository.LawMapper;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+	private final LawMapper lawMapper;
+	private final ItemMapper itemMapper;
+
+	public void save(ItemFormDto itemFormDto) {
+		LocalDateTime start = LocalDateTime.of(itemFormDto.getStartDate(), itemFormDto.getStartTime());
+		LocalDateTime end = LocalDateTime.of(itemFormDto.getEndDate(), itemFormDto.getEndTime());
+		ItemDto itemDto = new ItemDto(itemFormDto.getName(), itemFormDto.getCode(), itemFormDto.getType(),
+			itemFormDto.getPrice(), start, end,
+			Status.NOT_OPEN, "gunsan");
+		itemMapper.insertItem(itemDto);
+	}
+
+	public List<LawDto> findAllLaws() {
+		return lawMapper.findAllLaws();
+	}
+}
