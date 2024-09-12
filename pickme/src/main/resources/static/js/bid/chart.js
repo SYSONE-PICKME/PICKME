b0bb7da257a45b6a774aa88570777e0e0e1fb5ba
YangSoeun <soeun8636@naver.com>
2024-09-12 19:05:47 +0900

    [COMMENT]: 필요없는 주석 제거

--- pickme/src/main/resources/static/js/bid/chart.js
+++ pickme/src/main/resources/static/js/bid/chart.js
@@ -2,27 +2,27 @@
 const ctx = document.querySelector('#myChart').getContext('2d');
 
 let labels, data;
+
 if (allPrice.length > 0) {
     labels = allPrice.map((_, index) => index + 1); // x축 라벨은 1부터 시작하여 allPrice 크기만큼 설정
     data = allPrice.slice(); // y축 데이터는 allPrice로 설정
 } else {
-    labels = [1]; // x축 라벨을 1로 설정
-    data = [startPrice]; // y축 데이터는 startPrice로 설정
+    labels = [1];
+    data = [startPrice];
 }
 
-// x축 값 증가를 위한 변수
-let xValueCounter = labels.length + 1; // 다음 x축 라벨은 현재 라벨 크기 + 1
+let xValueCounter = labels.length + 1;
 
 // Chart.js를 이용해 차트 생성
 export const chart = new Chart(ctx, {
-    type: 'line', // 차트의 종류
+    type: 'line',
     data: {
-        labels, // x축에 해당하는 라벨을 빈 배열로 설정
+        labels,
         datasets: [{
-            label: '', // 데이터셋의 라벨을 빈 문자열로 설정
-            data, // y축 데이터를 빈 배열로 설정
-            backgroundColor: 'rgb(246,255,248)', //그래프 색상을 설정
-            borderColor: 'rgb(46,196,79)', // 테두리 색상을 설정
+            label: '',
+            data,
+            backgroundColor: 'rgb(246,255,248)',
+            borderColor: 'rgb(46,196,79)',
             borderWidth: 3,
         }]
     },
@@ -31,12 +31,12 @@ export const chart = new Chart(ctx, {
         maintainAspectRatio: false, // 차트의 가로 세로 비율을 유지하지 않음
 
         animation: {
-            duration: 100, // 애니메이션 지속 시간
+            duration: 500, // 애니메이션 지속 시간
             easing: 'ease-in-out' // 애니메이션의 변화 속도
         },
         plugins: {
             legend: {
-                display: false // 범례 표시 안함
+                display: false
             },
             title: {
                 display: true,
@@ -45,7 +45,7 @@ export const chart = new Chart(ctx, {
         }
     }
 });
-// Chart.js의 API를 이용해 차트에 새로운 데이터를 추가하는 함수
+
 export function addData(data) {
     chart.data.labels.push(xValueCounter++); //라벨 배열에 새로운 라벨을 추가
     chart.data.datasets[0].data.push(data); // 데이터 배열에 새로운 데이터를 추가
