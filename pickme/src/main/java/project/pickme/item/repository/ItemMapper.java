7fa06a1dfc11f37b3e8c42c1efe526e54b491df1
hamin-yeong <mytokki0323@gmail.com>
2024-09-12 21:40:22 +0900

    [FEAT]: Repository(Mapper클래스)구현

--- pickme/src/main/java/project/pickme/item/repository/ItemMapper.java
+++ pickme/src/main/java/project/pickme/item/repository/ItemMapper.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 
 import project.pickme.item.domain.Item;
+import project.pickme.item.dto.ItemDto;
 
 @Mapper
 public interface ItemMapper {
 	Optional<Item> findItemById(Long itemId);
+
+	void insertItem(ItemDto item);
 }
