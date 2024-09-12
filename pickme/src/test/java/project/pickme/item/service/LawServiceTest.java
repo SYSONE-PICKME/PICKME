96feb8c0181b84e260931f1e97a68a8e1e483cef
hamin-yeong <mytokki0323@gmail.com>
2024-09-12 09:25:11 +0900

    [REFACTOR]: 공백제거

--- pickme/src/test/java/project/pickme/item/service/LawServiceTest.java
+++ pickme/src/test/java/project/pickme/item/service/LawServiceTest.java
@@ -1,16 +1,11 @@
 package project.pickme.item.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import project.pickme.item.domain.LawDTO;
+import project.pickme.item.dto.LawDto;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -20,7 +15,7 @@ class LawServiceTest {
 
 	@Test
 	void getLaws() {
-		List<LawDTO> laws = lawService.getLaws();
+		List<LawDto> laws = lawService.getLaws();
 		System.out.println(laws.size());
 
 	}
