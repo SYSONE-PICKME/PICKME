7d49b57995b26d9452b4fa9d0d6b8df1b8294f00
hamin-yeong <mytokki0323@gmail.com>
2024-09-12 21:43:53 +0900

    [FEAT]: Category의 code부분 int형으로 수정

--- pickme/src/main/java/project/pickme/item/domain/Category.java
+++ pickme/src/main/java/project/pickme/item/domain/Category.java
@@ -4,16 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum Category {
-	CLOTHES("01", "의류"),
-	DAILY("02", "생활용품"),
-	DIGITAL("03", "디지털"),
-	FURNITURE("04", "가구"),
-	ETC("10", "기타");
+	CLOTHES(1, "의류"),
+	DAILY(2, "생활용품"),
+	DIGITAL(3, "디지털"),
+	FURNITURE(4, "가구"),
+	ETC(10, "기타");
 
-	private final String code;
+	private final int code;
 	private final String name;
 
-	Category(String code, String name) {
+	Category(int code, String name) {
 		this.code = code;
 		this.name = name;
 	}
