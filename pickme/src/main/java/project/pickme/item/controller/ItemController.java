e8d80fd26eab8bbec2b203bf214f6ac5cd7faef5
hamin-yeong <mytokki0323@gmail.com>
2024-09-12 09:46:51 +0900

    [REFACTOR]: Controller에 있던 Service로직 서비스레이어로 옮김

--- pickme/src/main/java/project/pickme/item/controller/ItemController.java
+++ pickme/src/main/java/project/pickme/item/controller/ItemController.java
@@ -1,8 +1,4 @@
-package project.pickme.item;
-
-import java.time.LocalDateTime;
-
-import java.util.List;
+package project.pickme.item.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,14 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-import project.pickme.item.constant.Status;
 import project.pickme.item.domain.Category;
-import project.pickme.item.dto.ItemDto;
 import project.pickme.item.dto.ItemFormDto;
-import project.pickme.item.dto.LawDto;
 import project.pickme.item.service.ItemService;
-import project.pickme.item.service.LawService;
-import project.pickme.item.service.S3Service;
 import project.pickme.user.constant.Type;
 
 @Controller
@@ -27,31 +18,22 @@ import project.pickme.user.constant.Type;
 @RequiredArgsConstructor
 public class ItemController {
 
-	private final LawService lawService;
 
 	private final ItemService itemService;
 
-	private final S3Service s3Service;
 
 	@GetMapping("/create")
 	public String getItem(Model model) {
-		List<LawDto> laws = lawService.getLaws();
 		model.addAttribute("item", new ItemFormDto());
-		model.addAttribute("laws", laws);
+		model.addAttribute("laws", itemService.findAllLaws());
 		model.addAttribute("type", Type.values());
 		model.addAttribute("categories", Category.values());
 		return "create-item";
 	}
 
 	@PostMapping("/create")
-	public String insertItem(@ModelAttribute ItemFormDto itemFormDTO) {
-		LocalDateTime start = LocalDateTime.of(itemFormDTO.getStartDate(), itemFormDTO.getStartTime());
-		LocalDateTime end = LocalDateTime.of(itemFormDTO.getEndDate(), itemFormDTO.getEndTime());
-		ItemDto itemDTO = new ItemDto(itemFormDTO.getName(), itemFormDTO.getCode(), itemFormDTO.getTarget(),
-			itemFormDTO.getPrice(), start, end,
-			Status.NOT_OPEN, "gunsan");
-
-		itemService.save(itemDTO);
+	public String insertItem(@ModelAttribute ItemFormDto itemFormDto) {
+		itemService.save(itemFormDto);
 		return "redirect:/customs/item/create";
 	}
 
--- pickme/src/main/java/project/pickme/item/service/ItemService.java
+++ pickme/src/main/java/project/pickme/item/service/ItemService.java
@@ -1,23 +1,38 @@
 package project.pickme.item.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
+import project.pickme.item.constant.Status;
 import project.pickme.item.dto.ItemDto;
-import project.pickme.item.repository.ItemMapper;
+import project.pickme.item.dto.ItemFormDto;
+import project.pickme.item.dto.LawDto;
+import project.pickme.item.mapper.ItemMapper;
+import project.pickme.item.mapper.LawMapper;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
-
-
+	private final LawMapper lawMapper;
 	private final ItemMapper itemMapper;
 
 	public void delete(Long id) {
 		itemMapper.delete(id);
 	}
 
-	public void save(ItemDto item) {
-		itemMapper.insertItem(item);
+	public void save(ItemFormDto itemFormDto) {
+		LocalDateTime start = LocalDateTime.of(itemFormDto.getStartDate(), itemFormDto.getStartTime());
+		LocalDateTime end = LocalDateTime.of(itemFormDto.getEndDate(), itemFormDto.getEndTime());
+		ItemDto itemDto = new ItemDto(itemFormDto.getName(), itemFormDto.getCode(), itemFormDto.getTarget(),
+			itemFormDto.getPrice(), start, end,
+			Status.NOT_OPEN, "gunsan");
+		itemMapper.insertItem(itemDto);
+	}
+
+	public List<LawDto> findAllLaws() {
+		return lawMapper.findAllLaws();
 	}
 
 	public void update(ItemDto item) {
@@ -28,5 +43,5 @@ public class ItemService {
 		return itemMapper.findById(id);
 	}
 
-
 }
+
