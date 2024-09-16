3c30b6a966643851d0a0150d2ac7ec5df71d6a8f
hamin-yeong <mytokki0323@gmail.com>
2024-09-16 16:51:48 +0900

    [REFACTOR]: date,time부분 datetime으로 변경 및 리팩토링

--- pickme/src/main/java/project/pickme/item/dto/ItemFormDto.java
+++ pickme/src/main/java/project/pickme/item/dto/ItemFormDto.java
@@ -1,7 +1,8 @@
 package project.pickme.item.dto;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,13 +15,12 @@ import project.pickme.user.constant.Type;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ItemFormDto {
+	private MultipartFile[] files;
 	private String name;
 	private int code;
 	private Type type;
 	private long price;
-	private LocalDate startDate;
-	private LocalTime startTime;
-	private LocalDate endDate;
-	private LocalTime endTime;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
 	private long[] lawId;
 }
