4bb84a6a26770a129dc0c8560096971d27eb7e60
hamin-yeong <mytokki0323@gmail.com>
2024-09-16 16:48:55 +0900

    [REFACTOR]: s3내용 삭제

--- pickme/src/main/java/project/pickme/item/service/ItemService.java
+++ pickme/src/main/java/project/pickme/item/service/ItemService.java
@@ -1,11 +1,9 @@
 package project.pickme.item.service;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import project.pickme.item.constant.Status;
@@ -28,12 +26,10 @@ public class ItemService {
 
 	private final S3Service s3Service;
 
-	public void save(ItemFormDto itemFormDto, MultipartFile[] files, Customs customs) throws IOException {
-		LocalDateTime start = LocalDateTime.of(itemFormDto.getStartDate(), itemFormDto.getStartTime());
-		LocalDateTime end = LocalDateTime.of(itemFormDto.getEndDate(), itemFormDto.getEndTime());
+	public void save(ItemFormDto itemFormDto, Customs customs) throws IOException {
 
 		ItemDto itemDto = new ItemDto(itemFormDto.getName(), itemFormDto.getCode(), itemFormDto.getType(),
-			itemFormDto.getPrice(), start, end,
+			itemFormDto.getPrice(), itemFormDto.getStartTime(), itemFormDto.getEndTime(),
 			Status.NOT_OPEN, customs.getId());
 
 		itemMapper.insertItem(itemDto);
@@ -42,7 +38,7 @@ public class ItemService {
 			ItemLawDto itemLawDto = new ItemLawDto(id, itemDto.getItemId());
 			itemLawMapper.insertLaw(itemLawDto);
 		}
-		s3Service.uploadImages(itemDto, files);
+		s3Service.uploadImages(itemDto, itemFormDto.getFiles());
 	}
 
 	public List<LawDto> findAllLaws() {
