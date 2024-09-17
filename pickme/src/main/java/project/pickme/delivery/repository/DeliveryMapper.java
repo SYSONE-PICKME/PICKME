1434b9480a510f270211483d01f004740857a1d8
hamin-yeong <mytokki0323@gmail.com>
2024-09-17 15:39:16 +0900

    [FEAT]: 송장이 등록된 ItemId를 가져오는 쿼리 작성

---
+++ pickme/src/main/java/project/pickme/delivery/repository/DeliveryMapper.java
@@ -0,0 +1,10 @@
+package project.pickme.delivery.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DeliveryMapper {
+	List<Long> registeredInvoiceItemId();
+}
