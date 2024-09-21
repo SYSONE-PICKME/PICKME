85c591c370380732b1d8d346507e012da5216bd8
hamin-yeong <mytokki0323@gmail.com>
2024-09-22 03:44:23 +0900

    [FEAT]: 배송 수정을 위한 controller와 restcontroller

--- pickme/src/main/java/project/pickme/item/controller/ItemController.java
+++ pickme/src/main/java/project/pickme/item/controller/ItemController.java
@@ -6,14 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import project.pickme.common.annotation.CurrentUser;
 import project.pickme.item.constant.Category;
 import project.pickme.item.dto.ItemFormDto;
+import project.pickme.item.dto.UpdateItemFormDto;
 import project.pickme.item.service.ItemService;
 import project.pickme.user.constant.Type;
 import project.pickme.user.domain.Customs;
@@ -50,9 +51,17 @@ public class ItemController {
 		return "/item/customsItems";
 	}
 
-	@GetMapping("/update")
-	public String showStatusPage(@RequestParam("itemId") Long itemId, Model model) {
-		model.addAttribute("itemId", itemId);
-		return "/item/updateItem";
+	@GetMapping("/edit/{id}")
+	public String editItemForm(@PathVariable long id, Model model) {
+		model.addAttribute("selectedItem", itemService.getItemById(id));
+		model.addAttribute("selectedLaws", itemService.getLawsByItemId(id));
+		model.addAttribute("selectedImages", itemService.getImagesByItemId(id));
+		model.addAttribute("form", new UpdateItemFormDto());
+		model.addAttribute("id", id);
+
+		model.addAttribute("laws", itemService.findAllLaws());
+		model.addAttribute("types", Type.values());
+		model.addAttribute("categories", Category.values());
+		return "/item/editItem";
 	}
 }
---
+++ pickme/src/main/java/project/pickme/item/controller/ItemRestController.java
@@ -0,0 +1,32 @@
+package project.pickme.item.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import project.pickme.common.annotation.CurrentUser;
+import project.pickme.item.dto.UpdateItemFormDto;
+import project.pickme.item.service.ItemService;
+import project.pickme.user.domain.Customs;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/customs")
+public class ItemRestController {
+	private final ItemService itemService;
+
+	@PatchMapping("/edit/{id}")
+	public ResponseEntity<?> updateItem(@PathVariable("id") long id,
+		@RequestBody UpdateItemFormDto form, @CurrentUser Customs customs) {
+		try {
+			itemService.updateItemByItemId(customs, form, id);
+			return ResponseEntity.ok().body("경매가 성공적으로 수정되었습니다.");
+		} catch (Exception e) {
+			return ResponseEntity.status(500).body("경매 수정에 실패했습니다: " + e.getMessage());
+		}
+	}
+}
