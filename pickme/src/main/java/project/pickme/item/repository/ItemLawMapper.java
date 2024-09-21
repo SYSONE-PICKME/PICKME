0a4f2249d9bba17e2a4ba7aa17cd2b5209c9cc9a
hamin-yeong <mytokki0323@gmail.com>
2024-09-22 03:41:54 +0900

    [FEAT]: 배송 수정을 위한 매퍼 연결

--- pickme/src/main/java/project/pickme/item/repository/ItemLawMapper.java
+++ pickme/src/main/java/project/pickme/item/repository/ItemLawMapper.java
@@ -1,5 +1,7 @@
 package project.pickme.item.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import project.pickme.item.dto.ItemLawDto;
@@ -7,4 +9,8 @@ import project.pickme.item.dto.ItemLawDto;
 @Mapper
 public interface ItemLawMapper {
 	void insertLaw(ItemLawDto itemLawDto);
+
+	void deleteLaw(long itemId);
+
+	List<Long> findLawsByItemId(long itemId);
 }
--- pickme/src/main/java/project/pickme/item/repository/ItemMapper.java
+++ pickme/src/main/java/project/pickme/item/repository/ItemMapper.java
@@ -2,9 +2,13 @@ package project.pickme.item.repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
 import project.pickme.item.dto.ItemDto;
+import project.pickme.item.dto.OriginalItemDto;
 import project.pickme.item.dto.SuccessfullCustomsItemDto;
+import project.pickme.item.dto.UpdateDto;
 
 @Mapper
 public interface ItemMapper {
@@ -15,4 +19,10 @@ public interface ItemMapper {
 	List<SuccessfullCustomsItemDto> findItemsByCustomsId(String customsId);
 
 	void updateStatus(LocalDateTime now);
+
+	void updateItem(UpdateDto updateDto);
+
+	OriginalItemDto findItemByItemId(long itemId);
+
+	String[] findImagesByItemId(long itemId);
 }
