a7a804d35cfd44765275fe54cba90d5d114fc332
Sohottoday <human9062@naver.com>
2024-09-21 18:16:44 +0900

    [REFACTOR]: 각 게시판 불러올 때 TYPE에 맞게 불러오도록 분리

--- pickme/src/main/java/project/pickme/notice/mapper/NoticeMapper.java
+++ pickme/src/main/java/project/pickme/notice/mapper/NoticeMapper.java
@@ -12,7 +12,6 @@ public interface NoticeMapper {
 
 	List<Notice> selectAllNotices();
 	List<Notice> selectAllCampaigns();
-	List<Notice> selectByType(@Param("type") String type);
 	Notice selectById(Long id);
 	void insert(Notice notice);
 	void update(Notice notice);
