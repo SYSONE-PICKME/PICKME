ca1cfae1abf10adf3e9d06bdb653d99ec59906b3
YangSoeun <soeun8636@naver.com>
2024-09-23 11:22:51 +0900

    [COMMENT]: UserRestController Javadoc 주석 작성

--- pickme/src/main/java/project/pickme/user/controller/UserRestController.java
+++ pickme/src/main/java/project/pickme/user/controller/UserRestController.java
@@ -3,7 +3,6 @@ package project.pickme.user.controller;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,6 +25,12 @@ import project.pickme.user.service.UserService;
 public class UserRestController {
 	private final UserService userService;
 
+	/**
+	 * 아이디 중복확인 메서드입니다.
+	 * 
+	 * @param id 중복확인 할 아이디
+	 * @return 중복확인 결과 메세지
+	 */
 	@PostMapping("/check-id")
 	public BaseResponse<?> checkDuplicateUserId(@RequestBody String id) {
 		userService.checkDuplicateId(id);
@@ -33,6 +38,13 @@ public class UserRestController {
 		return BaseResponse.ok("사용 가능한 아이디");
 	}
 
+	/**
+	 * 개인정보를 수정하는 메서드입니다.
+	 * 
+	 * @param updateInfoDto 수정할 개인정보를 담은 dto
+	 * @param user 로그인 된 사용자
+	 * @return 성공 여부
+	 */
 	@PutMapping("/info")
 	public BaseResponse<?> updateInfo(@RequestBody UpdateInfoDto updateInfoDto, @CurrentUser User user){
 		userService.updateInfo(updateInfoDto, user);
@@ -40,6 +52,13 @@ public class UserRestController {
 		return BaseResponse.ok();
 	}
 
+	/**
+	 * 비밀번호를 수정하는 메서드입니다.
+	 * 
+	 * @param updatePasswordDto 현재 비밀번호, 새로운 비밀번호를 담은 dto
+	 * @param user 로그인 된 사용자
+	 * @return 성공 여부
+	 */
 	@PutMapping("/password")
 	public BaseResponse<?> updatePassword(@RequestBody UpdatePasswordDto updatePasswordDto, @CurrentUser User user){
 		userService.updatePassword(updatePasswordDto, user);
@@ -47,6 +66,13 @@ public class UserRestController {
 		return BaseResponse.ok();
 	}
 
+	/**
+	 * 포인트 사용 내역을 조회하는 메서드입니다.
+	 * 
+	 * @param user 로그인 된 사용자
+	 * @param pageable 페이지네이션 정보
+	 * @return 포인트 사용내역, 누적합계 정보
+	 */
 	@GetMapping("/point/history")
 	public BaseResponse<?> showPointHistory(@CurrentUser User user, @PageableDefault(size = 6, page = 0) Pageable pageable) {
 		Page<PointHistoryDto> pointHistories = userService.showHistory(user, pageable);
