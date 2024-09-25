381e0ec2637fdaf5f9549cf015d04ae192c7ed4a
Sohottoday <human9062@naver.com>
2024-09-25 21:52:21 +0900

    [REFACTOR]: CustomsMainRestController javadoc 작성

--- pickme/src/main/java/project/pickme/notice/controller/CustomsMainRestController.java
+++ pickme/src/main/java/project/pickme/notice/controller/CustomsMainRestController.java
@@ -20,16 +20,35 @@ public class CustomsMainRestController {
 
 	private final CustomsMainService customsMainService;
 
+	/**
+	 * 최근 공지사항과 캠페인을 함께 조회합니다.
+	 * 최신 기준 6개의 공지사항과 캠페인 반환
+	 *
+	 * @return "notices"와 "campaigns" 키를 가진 Map이며
+	 * 			각각의 값은 NoticeDto와 CampaignDto의 리스트
+	 */
 	@GetMapping("/recent-all")
 	public BaseResponse<Map<String, List<?>>> getRecentAll() {
 		return BaseResponse.ok(customsMainService.getRecentAll());
 	}
 
+	/**
+	 * 최근 공지사항만을 조회
+	 * 최신 6개의 공지사항을 반환
+	 *
+	 * @return BaseResponse 객체에 감싸진 NoticeDto 리스트
+	 */
 	@GetMapping("/recent-notices")
 	public BaseResponse<List<NoticeDto>> getRecentNotices() {
 		return BaseResponse.ok(customsMainService.getRecentNotices());
 	}
 
+	/**
+	 * 최근 캠페인만을 조회
+	 * 최신 6개의 캠페인을 반환
+	 *
+	 * @return BaseResponse 객체에 감싸진 CampaignDto 리스트
+	 */
 	@GetMapping("/recent-campaigns")
 	public BaseResponse<List<CampaignDto>> getRecentCampaigns() {
 		return BaseResponse.ok(customsMainService.getRecentCampaigns());
