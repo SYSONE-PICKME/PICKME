8b4e27879364513b495e3e9cc51c831fc0cb9b00
YangSoeun <soeun8636@naver.com>
2024-09-25 14:32:51 +0900

    [FEAT]: 내 결제 목록 페이지네이션 적용

--- pickme/src/main/resources/static/js/bid/successfulBidList.js
+++ pickme/src/main/resources/static/js/bid/successfulBidList.js
@@ -2,23 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
     loadPaymentList()
 });
 
-function loadPaymentList() {
+function loadPaymentList(page, size) {
     $.ajax({
         url: '/user/bid/successful-list',
         method: 'GET',
+        data: { page: page, size: size },
         success: function (response) {
-            let source = document.getElementById('payment-template').innerHTML;
-            let template = Handlebars.compile(source);
-
-            let data = {
-                successfulBidList: response.data
-            };
-
-            let html = template(data);
-            $('#product-grid').html(html);
+            updatePaymentListContent(response.data.content)
+            updatePagination(response.data, 'loadPaymentList', size);
         },
         error: function (error) {
             console.error("결제 목록을 불러오는 중 오류가 발생했습니다:", error);
         }
     });
+}
+
+function updatePaymentListContent(payments){
+    let source = document.getElementById('payment-template').innerHTML;
+    let template = Handlebars.compile(source);
+
+    let data = { payments: payments };
+    let html = template(data);
+
+    $('#product-grid').html(html);
 }
