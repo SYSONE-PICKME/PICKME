764e9d58d7894a3b7788c9270b557af592737797
kys0411 <32180846@dankook.ac.kr>
2024-09-26 11:06:58 +0900

    [REFACTOR]: 메서드 순서 변경

--- pickme/src/main/java/project/pickme/item/controller/FindUserItemRestController.java
+++ pickme/src/main/java/project/pickme/item/controller/FindUserItemRestController.java
@@ -25,17 +25,17 @@ public class FindUserItemRestController {
 	private final static int DEFAULT_PAGE_SIZE = 5;
 	private final FindItemService itemService;
 
-	@GetMapping("/wish-list")
-	public BaseResponse<?> getWishList(@CurrentUser User user, @PageableDefault(size = DEFAULT_PAGE_SIZE, page = 0) Pageable pageable) {
-		Page<FindItemDto.WishList> wishList = itemService.findWishList(user.getId(), pageable);
-
-		return BaseResponse.ok(wishList);
-	}
-
 	@GetMapping("/list")
 	public BaseResponse<List<FindItemDto.GetAll>> getAllItems(@CurrentUser User user, @ModelAttribute ItemRequest.Cursor cursor) {
 		List<FindItemDto.GetAll> items = itemService.findAll(user.getId(), cursor);
 
 		return BaseResponse.ok(items);
+	}
+
+	@GetMapping("/wish-list")
+	public BaseResponse<?> getWishList(@CurrentUser User user, @PageableDefault(size = DEFAULT_PAGE_SIZE, page = 0) Pageable pageable) {
+		Page<FindItemDto.WishList> wishList = itemService.findWishList(user.getId(), pageable);
+
+		return BaseResponse.ok(wishList);
 	}
 }
